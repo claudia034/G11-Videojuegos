2e956ef6a4c36093c01dbf7b2473c5c0cff7c23b
acalidonio <116671236+acalidonio@users.noreply.github.com>
2026-06-21 02:49:01 -0600

    Feat: crear nueva entidad de PlayerStats que maneja las stats de un jugador individual

---
+++ tournament/src/main/java/com/tournament/domain/repository/PlayerStatsRepository.java
@@ -0,0 +1,12 @@
+package com.tournament.domain.repository;
+
+import com.tournament.domain.entity.PlayerStats;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PlayerStatsRepository extends JpaRepository<PlayerStats, Long> {
+    Optional<PlayerStats> findByPlayerId(Long playerId);
+}
