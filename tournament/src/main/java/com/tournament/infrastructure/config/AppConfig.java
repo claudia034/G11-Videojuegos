0b37f958722af025fe05c72bd07f9d8f01069697
clau034 <00037221@uca.edu.sv>
2026-06-28 00:47:55 -0600

    build: add backend local profile and runtime config

--- tournament/src/main/java/com/tournament/infrastructure/config/AppConfig.java
+++ tournament/src/main/java/com/tournament/infrastructure/config/AppConfig.java
@@ -14,6 +14,6 @@ public class AppConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("popularGames");
+        return new ConcurrentMapCacheManager("popularGames", "popularTournamentsByGame");
     }
 }
